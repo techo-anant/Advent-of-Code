1f16152b9b0c5673ace55f46306950be674e099c
Anant Kumar Singh <anantk.singh9876@gmail.com>
2025-12-01 02:09:12 -0500

    Soln to Day 1 AOC 2025

---
+++ Advent-of-Code-2025/Day1/p2.py
@@ -0,0 +1,27 @@
+start = 50
+num_0 = 0
+
+with open('input.txt', 'r') as f:
+    lines = f.read().splitlines()
+
+for line in lines:
+
+    if line[0] == 'L':
+        prev = start
+        start -= int(''.join(list(line)[1:]))
+
+        if start == 0:
+            num_0 += 1
+        elif start < 0:
+            if prev != 0:
+                num_0 += 1
+            num_0 += abs(start)//100 
+            start = (100 - abs(start)%100) % 100
+        
+    else:
+        start += int(''.join(list(line)[1:]))
+        if start > 99:
+            num_0 += start // 100
+            start %= 100 
+
+print(num_0)
